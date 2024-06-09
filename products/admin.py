4a4dcc740188bfc3e4c9622b7129d5eebe645396
psaisuryatejareddy <saitejageneral@gmail.com>
2024-06-09 16:33:41 +0530

    upload files

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, offer
 
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code' , 'discount')
+
+#to add names for the admin product 
 class ProductAdmin(admin.ModelAdmin):
-    list_display('name','price', 'stock')
+    list_display = ('name','price', 'stock')
 
+admin.site.register(offer, OfferAdmin)
 admin.site.register(Product, ProductAdmin)
 
